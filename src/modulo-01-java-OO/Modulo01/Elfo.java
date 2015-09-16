079033f363116fd17cdca09357b24420cf4db8bb
Marcos Koch <marcos_koch@hotmail.com>
2015-09-16 16:31:57 -0300

    teste

--- src/modulo-01-java-OO/Modulo01/Elfo.java
+++ src/modulo-01-java-OO/Modulo01/Elfo.java
@@ -1,19 +1,19 @@
-public class Elfo {
-    private String nome;
-    private int flechas;
-    private int exp = 0;
+  public class Elfo {
+      private String nome;
+      private int flechas;
+      private int exp = 0;
 
-    public Elfo(String nome, int flechas) {
-  		this.nome = nome;
-  		this.flechas = flechas;
-  	}
+      public Elfo(String nome, int flechas) {
+    		this.nome = nome;
+    		this.flechas = flechas;
+    	}
 
-    public Elfo(String nome){
-      this(nome, 42);
-    }
+      public Elfo(String nome){
+        this(nome, 42);
+      }
 
-	  public void atirarFlecha(){
-      flechas--;
-      exp++;
-    }
-}
+  	      public void atirarFlecha(){
+        flechas--;
+        exp++;
+      }
+  }
