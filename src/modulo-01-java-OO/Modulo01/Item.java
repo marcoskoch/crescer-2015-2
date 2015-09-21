f9ed0df8f8f47812f774691001c69b5bc6d0cdaa
Marcos Koch <marcos_koch@hotmail.com>
2015-09-21 09:36:35 -0300

    feat(Exercício 1):Criado classe item.

---
+++ src/modulo-01-java-OO/Modulo01/Item.java
@@ -0,0 +1,12 @@
+
+public class Item
+{
+    private String descricao;
+    private int quantidade;
+
+    public Item(String descricao, int quantidade){
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+    }
+
+}
