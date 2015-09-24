7562af1f340fa16d7d5ec7940ef66941cb218f89
Marcos Koch <marcos_koch@hotmail.com>
2015-09-24 00:30:04 -0300

    feat(Exército de Elfos): Item um, alistar elfos verde e noturnos;

---
+++ src/modulo-01-java-OO/Modulo01/ExercitoDeElfo.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+
+
+public class ExercitoDeElfo {
+    private HashMap <String, Elfo> exercito;
+
+    public ExercitoDeElfo (){
+        this.exercito = new HashMap<>();
+    }
+
+    public void alistarUmElfo(Elfo elfo){
+      if((elfo instanceof ElfoNoturno || elfo instanceof ElfoVerde) && elfo.getNome() != null){
+          exercito.put(elfo.getNome(), elfo);
+      }
+    }
+
+    public HashMap getExercito(){
+        return exercito;
+    }
+
+    public boolean equals(Object obj){
+        ExercitoDeElfo tropa = (ExercitoDeElfo) obj;
+        return this.exercito.equals(tropa.getExercito());
+    }
+}
---
+++ src/modulo-01-java-OO/Modulo01/ExercitoDeElfoTest.java
@@ -0,0 +1,44 @@
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+public class ExercitoDeElfoTest {
+
+    @Test
+    public void exercitoAlistaUmElfoVerde(){
+        ExercitoDeElfo exercito = new ExercitoDeElfo();
+        ElfoVerde elfo = new ElfoVerde("Lanterna Verde");
+        exercito.alistarUmElfo(elfo);
+        assertEquals(elfo, exercito.getExercito().get("Lanterna Verde"));
+    }
+
+    @Test
+    public void exercitoAlistaUmElfoNoturno(){
+        ExercitoDeElfo exercito = new ExercitoDeElfo();
+        ElfoNoturno elfo = new ElfoNoturno("Blade");
+        exercito.alistarUmElfo(elfo);
+        assertEquals(elfo, exercito.getExercito().get("Blade"));
+    }
+
+    @Test
+    public void exercitoAlistaUmElfoNoturnoEUmElfoVerde(){
+        ExercitoDeElfo exercito = new ExercitoDeElfo();
+        ElfoNoturno elfo = new ElfoNoturno("Blade");
+        ElfoVerde elfo2 = new ElfoVerde("Lanterna Verde");
+        exercito.alistarUmElfo(elfo);
+        exercito.alistarUmElfo(elfo2);
+
+        HashMap <String,Elfo> exercitoEsperado = new HashMap<>();
+        exercitoEsperado.put("Blade", elfo);
+        exercitoEsperado.put("Lanterna Verde", elfo2);
+
+        assertEquals(exercitoEsperado, exercito.getExercito());
+    }
+
+
+
+}
