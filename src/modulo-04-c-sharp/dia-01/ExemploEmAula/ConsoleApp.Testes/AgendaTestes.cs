00442280ba46cf30b1b41dd0d12fb3bee4c760a7
Marcos Koch <marcos_koch@hotmail.com>
2015-10-27 21:44:34 -0200

    teste na agenda

--- src/modulo-04-c-sharp/dia-01/ExemploEmAula/ConsoleApp.Testes/AgendaTestes.cs
+++ src/modulo-04-c-sharp/dia-01/ExemploEmAula/ConsoleApp.Testes/AgendaTestes.cs
@@ -16,5 +16,37 @@ namespace ConsoleApp.Testes
 
             Assert.AreEqual(agenda.QuantidadeContatos, 1);
         }
+
+        [TestMethod]
+        public void AgendaCom3ContatosRemoverDoisContatosComMesmoNome()
+        {
+            var contato1 = new Contato()
+            {
+                Nome = "Fabricio",
+                Numero = 1515515
+            };
+
+            var contato2 = new Contato()
+            {
+                Nome = "Ben Hur",
+                Numero = 666
+            };
+
+            var contato3 = new Contato()
+            {
+                Nome = "Ben Hur",
+                Numero = 666
+            };
+
+            var agenda = new Agenda();
+            agenda.AdicionarContato(contato1);
+            agenda.AdicionarContato(contato2);
+            agenda.AdicionarContato(contato3);
+
+            agenda.RemoverContatosPorNome("Ben Hur");
+
+            Assert.AreEqual(agenda.QuantidadeContatos, 1);
+        }
     }
+
 }
